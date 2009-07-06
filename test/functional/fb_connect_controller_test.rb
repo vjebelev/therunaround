91a406beb4c6339e9dca0272a90807f712fc2568
Alexandr <s4ni.la@gmail.com>
2009-07-06 18:30:57 +0300

    Functionals tests for fb connect

--- test/functional/fb_connect_controller_test.rb
+++ test/functional/fb_connect_controller_test.rb
@@ -16,9 +16,9 @@ class FbConnectControllerTest < ActionController::TestCase
     assert_redirected_to @facebook_session.login_url
   end
 
-  def test_connect
+  def test_connect_should_redirect_to_main_page
+    @controller = FbConnectController.new
     assert_not_nil @facebook_session
-
     user = users(:first)
     user.update_attribute(:fb_uid, 123)
     @facebook_session.secure_with!("a session key", user.fb_uid, Time.now.to_i + 60)
@@ -26,18 +26,17 @@ class FbConnectControllerTest < ActionController::TestCase
 
     assert_not_nil @request
 
-
     @request.session[:user_id] = user.id
     @request.session[:facebook_session] = @facebook_session
 
     assert_not_nil @request.session[:facebook_session]
-    assert_not_nil @request.session[:facebook_session].session_key
     assert_equal user.fb_uid, @request.session[:facebook_session].user.uid
 
     setup_controller_request_and_response
-
+    @controller.send(:instance_variable_set, '@facebook_session', @facebook_session)
     post :connect
 
-    assert_response :success
+    assert_response :redirect
+    assert_redirected_to '/'
   end
 end
